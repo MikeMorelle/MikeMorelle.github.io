06aca9fe5e53af3416d99125a22d6e6f707f9a70
deniz <deniz-d-1012@web.de>
2026-07-21 10:59:51 +0200

    Adjustments

--- code/backend/app/core/config.py
+++ code/backend/app/core/config.py
@@ -9,7 +9,7 @@ Application configuration layer loads environment variables
 
 class Settings:
     S3_ENDPOINT = os.getenv("S3_ENDPOINT", "http://localhost:8333")
-    S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY", "set_value_here")
-    S3_SECRET_KEY = os.getenv("S3_SECRET_KEY", "set_value_here")
+    S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY", "admin")
+    S3_SECRET_KEY = os.getenv("S3_SECRET_KEY", "admin")
 
 settings = Settings()
